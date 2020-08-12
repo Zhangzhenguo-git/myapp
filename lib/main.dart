8685d3adb7fdfc414b985bc23f1488728de978be
Zhangzhenguo-git <826189539@qq.com>
2020-08-12 22:07:49 +0800

    1.添加TabBar（设置指示器大小，颜色，切换变化效果）。2.添加TabBarView并与TabBar进行联动。3.添加仿QQ侧滑栏（我的：头像个人描述，下方列表内容等）。4.添加底部导航栏BottomNavigationBar（BottomNavigationBarItem图标内容样式）。

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,31 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:myapp/listviewdemo/listview-demo.dart';
-import 'package:myapp/model/post.dart';
+import 'package:myapp/demo/listview-demo.dart';
+import 'package:myapp/demo/bottom_navigation_bar.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: '导航的数据传递和接受',
-//    定义商品列表页面组件进行传值
-    home: Home(),
-//    主题
-    theme: ThemeData(
-//      主样本
-        primarySwatch: Colors.yellow,
-        //    自定义按钮水波纹
-        highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
-//      透明度
-        splashColor: Colors.white70),
-  ));
+  runApp(App());
 }
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '导航的数据传递和接受',
+//    定义商品列表页面组件进行传值
+      home: Home(),
+//    主题
+      theme: ThemeData(
+//      主样本
+          primarySwatch: Colors.yellow,
+          //    自定义按钮水波纹
+          highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+//      透明度
+          splashColor: Colors.white70),
+    );
+  }
+}
+
 
 class Home extends StatelessWidget {
   @override
@@ -26,88 +34,119 @@ class Home extends StatelessWidget {
     return DefaultTabController(
         length: 3,
         child: Scaffold(
-            backgroundColor: Colors.grey[200],
-            appBar: AppBar(
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
 //        左侧AppBar功能图标
-                leading: IconButton(
-                  icon: Icon(Icons.menu),
-                  tooltip: 'Navigration',
-//          事件
-                  onPressed: () => debugPrint("Navigration button is pressed"),
-                ),
-                title: Text("Navigration"),
+              title: Text("Navigration"),
 //        右侧AppBar功能图标
-                actions: [
-                  IconButton(
-                      icon: Icon(Icons.search),
-                      onPressed: () => debugPrint('Search button is pressed'))
-                ],
-                elevation: 0.0,
-                bottom: TabBar(
-//                tabBar指示器未选中状态下颜色
-                  unselectedLabelColor: Colors.blue,
-//                tabBar指示器的底部颜色
-                  indicatorColor: Colors.red,
-//                tabBar指示器的宽度大小
-                  indicatorSize: TabBarIndicatorSize.label,
-//                tabBar指示器的高度
-                  indicatorWeight: 1.0,
-                  tabs: [
-                    Tab(
-                      icon: Icon(Icons.local_florist),
-                    ),
-                    Tab(
-                      icon: Icon(Icons.change_history),
-                    ),
-                    Tab(
-                      icon: Icon(Icons.directions_bike),
-                    )
-                  ],
-                )),
-            body: TabBarView(
-              children: [
-                Icon(
-                  Icons.local_florist,
-                  size: 128.0,
-                  color: Colors.black12,
-                ),
-                Icon(
-                  Icons.change_history,
-                  size: 128.0,
-                  color: Colors.black12,
-                ),
-                Icon(
-                  Icons.directions_bike,
-                  size: 128.0,
-                  color: Colors.black12,
-                ),
+              actions: [
+                IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () => debugPrint('Search button is pressed'))
               ],
-            ),
-//          Container小容器
-            drawer: Drawer(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  DrawerHeader(
-                    child: Text('header'.toUpperCase()),
-//                    背景样式
-                    decoration: BoxDecoration(
-                      color: Colors.grey[100]
-                    ),
+              elevation: 0.0,
+              bottom: TabBar(
+//                tabBar指示器未选中状态下颜色
+                unselectedLabelColor: Colors.blue,
+//                tabBar指示器的底部颜色
+                indicatorColor: Colors.red,
+//                tabBar指示器的宽度大小
+                indicatorSize: TabBarIndicatorSize.label,
+//                tabBar指示器的高度
+                indicatorWeight: 1.0,
+                tabs: [
+                  Tab(
+                    icon: Icon(Icons.local_florist),
                   ),
-                  ListTile(
-                    title: Text('message',textAlign: TextAlign.center,),
-//                    右侧图标
-                    trailing: Icon(Icons.message,color: Colors.yellow,),
+                  Tab(
+                    icon: Icon(Icons.change_history),
                   ),
-                  ListTile(
-                    title: Text('message',textAlign: TextAlign.center,),
-//                    左侧图标
-                    leading: Icon(Icons.message,color: Colors.lightBlue,),
+                  Tab(
+                    icon: Icon(Icons.directions_bike),
                   )
                 ],
+              )),
+          body: TabBarView(
+            children: [
+              MyListView(),
+              Icon(
+                Icons.change_history,
+                size: 128.0,
+                color: Colors.black12,
               ),
-            )));
+              Icon(
+                Icons.directions_bike,
+                size: 128.0,
+                color: Colors.black12,
+              ),
+            ],
+          ),
+//          Container小容器
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                UserAccountsDrawerHeader(
+                  accountName: Text(
+                    '用户名',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  accountEmail: Text('1234569...@qq.com'),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "http://img5.mtime.cn/mt/2020/07/24/084252.78274888_270X360X4.jpg"),
+                  ),
+//                    局部装饰
+                  decoration: BoxDecoration(
+                      color: Colors.yellow[400],
+                      borderRadius: BorderRadius.all(Radius.circular(0.0)),
+//                      背景图
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://fanyiapp.cdn.bcebos.com/cms/image/58eb5ccdedfd017daa2e7d4e48d43891.jpg'),
+                          fit: BoxFit.cover,
+                          colorFilter: ColorFilter.mode(
+                              Colors.yellow[400].withOpacity(0.6),
+//                            颜色的混合效果
+                              BlendMode.srcOver))),
+                ),
+//                  DrawerHeader(
+//                    child: Text('header'.toUpperCase()),
+////                    背景样式
+//                    decoration: BoxDecoration(
+//                      color: Colors.grey[100]
+//                    ),
+//                  ),
+                ListTile(
+                  title: Text(
+                    'message',
+                    textAlign: TextAlign.center,
+                  ),
+//                    右侧图标
+                  trailing: Icon(
+                    Icons.message,
+                    color: Colors.yellow,
+                  ),
+                  onTap: () => Navigator.pop(context),
+                ),
+                ListTile(
+                  title: Text(
+                    'message',
+                    textAlign: TextAlign.center,
+                  ),
+//                    左侧图标
+                  leading: Icon(
+                    Icons.message,
+                    color: Colors.lightBlue,
+                  ),
+                  onTap: () => Navigator.pop(context),
+                )
+              ],
+            ),
+          ),
+          //底部导航栏
+          bottomNavigationBar: BottomNavigationBarDemo(),
+        ));
   }
 }
 
