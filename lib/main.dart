45b6f4a57ae101426a30d7dae7962a74f03de910
Zhangzhenguo-git <826189539@qq.com>
2020-09-07 22:17:49 +0800

    添加TextView组件、Image组件-圆角-快速设置圆角、ListView列表

--- lib/main.dart
+++ lib/main.dart
@@ -1,163 +1,142 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:myapp/demo/listview-demo.dart';
-import 'package:myapp/demo/bottom_navigation_bar.dart';
 
 void main() {
-  runApp(App());
+  runApp(Main());
 }
-class App extends StatelessWidget {
+
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: '导航的数据传递和接受',
-//    定义商品列表页面组件进行传值
-      home: Home(),
-//    主题
-      theme: ThemeData(
-//      主样本
-          primarySwatch: Colors.yellow,
-          //    自定义按钮水波纹
-          highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
-//      透明度
-          splashColor: Colors.white70),
-    );
-  }
-}
-
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-//    TabBar标签形式
-    return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          backgroundColor: Colors.grey[100],
-          appBar: AppBar(
-//        左侧AppBar功能图标
-              title: Text("Navigration"),
-//        右侧AppBar功能图标
-              actions: [
-                IconButton(
-                    icon: Icon(Icons.search),
-                    onPressed: () => debugPrint('Search button is pressed'))
-              ],
-              elevation: 0.0,
-              bottom: TabBar(
-//                tabBar指示器未选中状态下颜色
-                unselectedLabelColor: Colors.blue,
-//                tabBar指示器的底部颜色
-                indicatorColor: Colors.red,
-//                tabBar指示器的宽度大小
-                indicatorSize: TabBarIndicatorSize.label,
-//                tabBar指示器的高度
-                indicatorWeight: 1.0,
-                tabs: [
-                  Tab(
-                    icon: Icon(Icons.local_florist),
-                  ),
-                  Tab(
-                    icon: Icon(Icons.change_history),
-                  ),
-                  Tab(
-                    icon: Icon(Icons.directions_bike),
-                  )
-                ],
-              )),
-          body: TabBarView(
-            children: [
-              MyListView(),
-              Icon(
-                Icons.change_history,
-                size: 128.0,
-                color: Colors.black12,
-              ),
-              Icon(
-                Icons.directions_bike,
-                size: 128.0,
-                color: Colors.black12,
-              ),
-            ],
-          ),
-//          Container小容器
-          drawer: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: [
-                UserAccountsDrawerHeader(
-                  accountName: Text(
-                    '用户名',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  accountEmail: Text('1234569...@qq.com'),
-                  currentAccountPicture: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        "http://img5.mtime.cn/mt/2020/07/24/084252.78274888_270X360X4.jpg"),
-                  ),
-//                    局部装饰
-                  decoration: BoxDecoration(
-                      color: Colors.yellow[400],
-                      borderRadius: BorderRadius.all(Radius.circular(0.0)),
-//                      背景图
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              'https://fanyiapp.cdn.bcebos.com/cms/image/58eb5ccdedfd017daa2e7d4e48d43891.jpg'),
-                          fit: BoxFit.cover,
-                          colorFilter: ColorFilter.mode(
-                              Colors.yellow[400].withOpacity(0.6),
-//                            颜色的混合效果
-                              BlendMode.srcOver))),
-                ),
-//                  DrawerHeader(
-//                    child: Text('header'.toUpperCase()),
-////                    背景样式
-//                    decoration: BoxDecoration(
-//                      color: Colors.grey[100]
-//                    ),
-//                  ),
-                ListTile(
-                  title: Text(
-                    'message',
-                    textAlign: TextAlign.center,
-                  ),
-//                    右侧图标
-                  trailing: Icon(
-                    Icons.message,
-                    color: Colors.yellow,
-                  ),
-                  onTap: () => Navigator.pop(context),
-                ),
-                ListTile(
-                  title: Text(
-                    'message',
-                    textAlign: TextAlign.center,
-                  ),
-//                    左侧图标
-                  leading: Icon(
-                    Icons.message,
-                    color: Colors.lightBlue,
-                  ),
-                  onTap: () => Navigator.pop(context),
-                )
-              ],
-            ),
-          ),
-          //底部导航栏
-          bottomNavigationBar: BottomNavigationBarDemo(),
+        home: Scaffold(
+          body: Home(),
         ));
   }
 }
 
-class Hello extends StatelessWidget {
+//首页展示各个组件
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('这是Hello小组件'),
-      ),
-      body: Center(),
+    return Row(
+      children: [
+        Column(
+          children: [MyText(), MyImage(), MyListView(),],
+        ),
+
+      ],
     );
   }
 }
+
+//文本组件
+class MyText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.red,
+        width: 100.0,
+        height: 100.0,
+        child: Text(
+          '各位同学大家好，欢迎来到flutter联盟',
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2,
+//          文字的大小倍数
+          textScaleFactor: 1.5,
+        ));
+  }
+}
+
+//图片组件-普通图片+圆角图片+ClipOveal快速圆角
+class MyImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 100,
+          height: 100,
+          child: Image.network(
+            "https://i0.hdslb.com/bfs/sycp/creative_img/202009/c19141dc0ac5502ca640c344c05a5d80.jpg@880w_388h_1c_95q",
+            alignment: Alignment.bottomRight,
+//        color和colorBlendMode图片混合颜色
+            color: Colors.yellow,
+            colorBlendMode: BlendMode.colorBurn,
+//        图片相对于容器的填充效果
+//        fit: BoxFit.cover,
+//        图片的平铺效果和fit不能同时使用
+//        repeat: ImageRepeat.repeat,
+          ),
+          decoration: BoxDecoration(
+              color: Colors.yellow,
+//        borderRadius: BorderRadius.all(Radius.circular(50)),
+              borderRadius: BorderRadius.circular(50)),
+        ),
+//        圆角图片
+        Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage(
+                    'https://i0.hdslb.com/bfs/sycp/creative_img/202009/c19141dc0ac5502ca640c344c05a5d80.jpg@880w_388h_1c_95q'),
+              )),
+        ),
+//        圆角
+        ClipOval(
+          child: Image.network(
+            'https://i0.hdslb.com/bfs/sycp/creative_img/202009/c19141dc0ac5502ca640c344c05a5d80.jpg@880w_388h_1c_95q',
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          child: Image.asset(
+            'images/111.jpg',
+            fit: BoxFit.cover,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class MyListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 160,
+      height: 300,
+      color: Colors.blue,
+      child: ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('列表标题ttttttttt'),
+            subtitle: Text('列表详细内容1111111111'),
+          ),
+          ListTile(
+            title: Text('列表标题tttttttttt'),
+            subtitle: Text('列表详细内容111111',
+//            超出宽度省略
+            overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          ListTile(
+            title: Text('列表标题ttttttttt'),
+            subtitle: Text('列表详细内容1111111111',
+            overflow:TextOverflow.ellipsis),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
